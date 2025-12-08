634f731bec38c7c9eef55b067f086294b6d83ff8
bigboy99-stack <helldogg42@gmail.com>
2025-12-08 06:45:53 -0500

    gh

--- apis/anyApi.js
+++
@@ -1,1 +0,0 @@
-<!-- an app that adds something to an api, then click to enter a another page that CRUD the same api -->
--- apis/rbac.mjs
+++
@@ -1,1 +0,0 @@
-// RBAC for netflix, spotify, as subcription tiers, in that any one that has paid will have access to a higher elite role 
